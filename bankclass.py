c9df342a2245dff1a8a665eeea5bd7236a0bbfc6
AI Apostle <ikhegbe9@gmail.com>
2025-12-10 09:03:08 +0100

    Improve on the classes

--- bankclass.py
+++ bankclass.py
@@ -1,6 +1,11 @@
 import sqlite3
 con = sqlite3.connect('bank.db')
 cur = con.cursor()
+import mysql.connector
+
+
+
+
 
 # this is the instance of the bank user
 class User:
@@ -22,14 +27,13 @@ class User:
         self.acct_no = acct_no
         self.balance = balance
         
-    def transfer(self,amount,target_acct,):
+    def transfer(self,amount,target_acct):
         if self.balance >= amount:
             new_balance = self.balance - amount
             print('transfer succesful')
             print(new_balance)
         else:
             print('insufficent funds')
-            print(self.balance)
         return self.balance
             
 
@@ -55,98 +59,155 @@ class User:
 
         return self.balance
 
-    def deposit(self):
-        amount = int(input("Enter the amount you want to send"))
-        if amount > 0:
+    def deposit(self,amount):
+
+        amount = int(input("Enter the amount you want to send___")).is_integer()
+        if amount >= 100:
             self.balance += amount
-        elif amount < 2:
+        elif amount < 100:
             print("can't transfer less than hundred naira")
         else:
             print('amount too small')
-        print(f'deposited {self.balance}')
+        return f'deposited {self.balance}'
         print(self.balance)
     def get_balance(self,balance):
         pass
+
+
 # this is the instance the of the bank class
 class Bank:
     def __init__(self,current_user,
                  db_connect):
         
         self.current_user = current_user
-        self.db_connect = db_connect
+        self.db_connect = db_connect 
+
     # this function is to create a bank account 
     def create_acct(self):
+
         while True:
             try:
-                id = 1 # maybe make this auto increasezsgggfe
-                name = str(input("Enter Your Name"))
-                email = str(input("Enter Email"))
-                phone = str(input("Enter Phone Number"))
-                password = int(input("enter password"))
-                pin = int(input('Enter your 4 digit pin'))
-                acct_no = phone[1:]
-                # i think this object of the user should happen after the user caalls the depoist function
-                #user_obj = User(id,name,email,phone,pin,password,acct_no)
+                full_name = str(input("Enter Your Name____"))
+                email_address = str(input("Enter Email____"))
+                phone_no = str(input("Enter Phone Number (e.g +2349045455676)____"))
+                pass_word = int(input("Enter Password(Enter only numbers)____"))
+                acct_no = phone_no[4:] 
+                pin = int(input('Enter your 4 digit pin_____'))
+                print(acct_no)
+                Database_manager.insert_user(self,full_name,email_address,phone_no,pass_word,acct_no,pin)
                 
-            except ValueError:
-                print('invalid entry')
+            except ValueError as v:
+                print(f'invalid entry{v}')
                 continue
-            except TypeError:
-                print('Invalid entry')
+            except TypeError as t:
+                print(f'Invalid entry {t}')
+                continue
+            except mysql.connector.Error as err:
+                print(f" User with this email already exist")
                 continue
             except Exception as e:
-                print('invalid entry')
+                print(f'invalid entry {e}')
                 continue
             else:
-
-
-                print('run sql statement')
-                print('run either deposit or exit')
                 break
+            return "You have Successfully Created an Account with Pybank"
             
     # this function helps to login into the bank account
     def login(self):
         while True:
             try:
-                login_acct_no = int(input("Enter your 10 digit Account Number......"))
-                login_password = int(input('Enter Your 5 digit Password........'))
-                try:
-                    # run sql statement here
-                    # select  acct_no, login_password from user where id = 1
-                    # 
-                    db_2=[12345]
-                    db_1= [12345]
-                    if login_acct_no in db_2 and login_password in db_1:
-                        #run sql statement
-                        print("Name found")
-                    else:
-                        print("Records Do Not Match")
-                except ConnectionError:
-                    print('name not found')
+                email_address = str(input("Enter your email address to login...."))
+                pass_word = int(input('Enter Your 6 digit Password to login........'))
+
             except ValueError:
                 print('invalid error')
                 continue
-            except TypeError:
-                print("Type error")
+            except TypeError as t:
+                print(f"Type error {t}")
                 continue
+            except mysql.connector.Error as err:
+                print(f"Database Error \nName not found {err}")
+                con
             except Exception as e:
-                print('plenty error')
+                print(f'plenty error{e}')
                 continue
             else:
-                print('do something here')
+                try:
+                   Database_manager.login_user(self,email_address,pass_word)
+                   Bank.show_main_main()
+                except Exception as e:
+                    print(e)
                 break
-    def show_main_main(self):
 
-        pass
+    def show_main_main(self):
+        main_main = int(input("1.Transfer\n" \
+                            "2.Withdraw\n" \
+                            "3.Deposit\n" \
+                            "4.Check Balance......."))
+        while True:
+            if main_main == 1:
+                break
+                print("you will transfer here ")
+            elif main_main == 2:
+                print("you withdraw here ")
+            elif main_main == 3:
+                print("you deposit here ")
+            elif main_main == 4:
+                print("check your balance ")
+            else:
+                print("you have exited ")
+                break
+    
+        
+
+
+        
     def handle_transaction(self):
         pass
 
+#this class is for housing all my sql queries
+class Database_manager:
+    def __init__(self,host,user,password,database):
+        self.connection =  mysql.connector.connect(
+            host = host,
+            user = user,
+            password = password,
+            database = database)
+        self.cursor = self.connection.cursor()
+        self.insert_user_query = "insert into users (full_name,email_address,phone_no,\
+                                        pass_word,acct_no,pin)" \
+                                        " values (%s,%s,%s,%s,%s,%s);"
+        
+        self.login_user_query = "select email_address,pass_word,  \
+                                case when email_address = %s and " \
+                                "pass_word = %s then 'login successfully' " \
+                                "else 'login unsuccessful' " \
+                                "end as login from users;"
+        user_transfer_query = " "
 
-def main(): 
-    john = User(1,'Acha Saviour','Acha@gmail.com',2349067860967,1094,102345, 9067860967 )
-    #john.deposit(2000)
-    john.withdraw(1000)
-    #john.transfer(1500,9067860967)
+    def insert_user(self,full_name,email_address,phone_no,pass_word,acct_no,pin):
+        self.cursor.execute(self.insert_user_query,(full_name,email_address,phone_no,pass_word,acct_no,pin))
+        self.connection.commit()
+        print('New Record Inserted into Database successfully')
+
+    def login_user(self,email_address,pass_word):
+        fetch_it = self.cursor.execute(self.login_user_query,(email_address,pass_word))
+
+    
+
+            
+
+
+
+def main(self): 
+   # insert_user = "insert into users values ('Acha Saviour','acha@gmail.com', '+2349067860967', '344769', 9067860967, '2003');"
+    #db = Database_manager()
+    
+
+
+    print('hello world')
+    
+
 
 if __name__ == "__main__":
     main()
--- testing.py
+++ testing.py
@@ -1,29 +1,46 @@
+# this imports the user and bank class from bankclass.py
 from bankclass import User 
 from bankclass import Bank
+from bankclass import Database_manager
+
+HOST = 'localhost'
+USER = 'root'
+PASSWORD = 'Acha@004'
+DATABASE = 'pybankDB'
+
+db = Database_manager(HOST,USER,PASSWORD,DATABASE)
+#db.insert_user('Acha Saviour Ikhamate','ach123a@gmail.com', '+2349067860968', '344767', 9067860966, '2004')
+
+
+'''
+ this while loop keeps running untill a user types in a number between 1 and 3 and doesn't accpets letters, but if a number other than 1-3 is enter it exits
+'''
+
+
 while True:
     try:
         option = int(input('welcome to pybank.....\
                         \n1. Create Account\
                         \n2. Login\
-                        \n3. Exit'))
+                        \n3. Exit__________'))
+        
         if option == 1:
-            Bank.create_acct()
-            first_deposit = str(input("Do you want to deposit Yes/No"))
-            if first_deposit == "yes":
-                new_user = User(id,"name","email","phone","pin","password","acct_no")
-                new_user.deposit()
-                print("we run deposit here")
-            else:
-                break
+
+            # this calls the create account function in the  bank class in bankclass.py 
+            Bank.create_acct(db)
+            #Bank.login(db)
 
         elif option == 2:
-            Bank.login()
+           # Bank.login(db)
+           # Bank.show_main_main(db)
+           Bank.show_main_main(db)
+
         else:
-            print('Exited PyBank')
             break
+            print('Exited PyBank')
         
     except ValueError:
-        print('invalid entry')
+        print('Invalid entry\nEnter only 1-3')
         continue
     else:
         break
